e4b3f4e69c1092f17944ab957510c5cdf05510ec
ebremner <ebremner@uwaterloo.ca>
2019-09-18 22:23:01 -0400

    Adding corps info settings

--- src/Form/SiteSettingsForm.php
+++ src/Form/SiteSettingsForm.php
@@ -35,14 +35,33 @@ class SiteSettingsForm extends ConfigFormBase {
 
     $config = $this->config('cc_site_controller.sitesettings');
 
-    $form['site_title'] = [
-      '#title' => t('Site title'),
-      '#type' => 'textfield',
+    $form['type_of_cadets'] = [
+      '#title' => t('Type of Cadets'),
+      '#type' => 'select',
+      '#options' => [
+        'air' => $this->t('Air'),
+        'army' => $this->t('Army'),
+        'sea' => $this->t('Sea'),
+      ],
+      '#default_value' => $config->get('type_of_cadets') !== NULL ? $config->get('type_of_cadets') : '',
     ];
 
-    $form['type_of_site'] = [
+    $form['corps_number'] = [
+      '#title' => t('Corps Number'),
+      '#type' => 'textfield',
+      '#default_value' => $config->get('corps_number'),
+    ];
+
+    $form['corps_name'] = [
+      '#title' => t('Corps Name'),
+      '#type' => 'textfield',
+      '#default_value' => $config->get('corps_name'),
+      '#description' => t('Enter the corps name, without the RCSCC, RCACS or RCACC'),
+    ];
+
+    $form['cadet_theme'] = [
       '#type' => 'select',
-      '#title' => $this->t('Type of site'),
+      '#title' => $this->t('Theme of site'),
       '#options' => [
         'air-cadets' => $this->t('Air'),
         'army-cadets' => $this->t('Army'),
@@ -50,7 +69,7 @@ class SiteSettingsForm extends ConfigFormBase {
         'generic' => $this->t('Generic'),
         'rcscc-warspite' => $this->t('RCSCC Warspite'),
       ],
-      '#default_value' => $config->get('type_of_site') !== NULL ? $config->get('type_of_site') : 'generic',
+      '#default_value' => $config->get('cadet_theme') !== NULL ? $config->get('cadet_theme') : 'generic',
     ];
 
     return parent::buildForm($form, $form_state);
@@ -63,13 +82,18 @@ class SiteSettingsForm extends ConfigFormBase {
 
     parent::submitForm($form, $form_state);
 
-    $this->config('cc_site_controller.sitesettings')
-      ->set('site_title', $form_state->getValue('site_title'))
-      ->save();
+    $values_to_process = [
+      'type_of_cadets',
+      'corps_number',
+      'corps_name',
+      'cadet_theme',
+    ];
 
-    $this->config('cc_site_controller.sitesettings')
-      ->set('type_of_site', $form_state->getValue('type_of_site'))
-      ->save();
+    foreach ($values_to_process as $value_to_process) {
+      $this->config('cc_site_controller.sitesettings')
+        ->set($value_to_process, $form_state->getValue($value_to_process))
+        ->save();
+    }
   }
 
 }
